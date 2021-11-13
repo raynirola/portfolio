51d690eeee25b830478bdc4d0c52ce7e65172542
Ray Nirola <hi@raynirola.com>
2021-11-14 00:56:49 +0530

    toast on toogle

--- components/ThemeToggleButton.tsx
+++ components/ThemeToggleButton.tsx
@@ -1,14 +1,25 @@
 import { useEffect, useState } from 'react'
 import { MoonIcon, SunIcon } from '@heroicons/react/solid'
 import { useTheme } from 'next-themes'
+import { useNotification } from '@/contexts/NotificationContext'
 
 const ThemeToggleButton = () => {
   const [mounted, setMounted] = useState(false)
   const { resolvedTheme, setTheme } = useTheme()
+  const { addNotification } = useNotification()
 
   useEffect(() => setMounted(true), [])
 
-  const handleClick = () => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+  const handleClick = () => {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+    addNotification({
+      title: 'Theme changed',
+      message: `Theme changed to ${
+        resolvedTheme === 'dark' ? 'light' : 'dark'
+      } and saved to local storage lorem ipsum dolor sit amet`,
+      type: 'info',
+    })
+  }
 
   return (
     <button
