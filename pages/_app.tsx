d5a6db19917391dafca8c2ccb13c488b9f804aea
Ray Nirola <hi@raynirola.com>
2021-11-14 00:55:58 +0530

    QueryContextProvide, NotificationProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
+import '@/styles/app.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-import '@/styles/app.css'
+import { NotificationProvider } from '@/contexts/NotificationContext'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class">
+        <NotificationProvider>
+          <Component {...pageProps} />
+        </NotificationProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
