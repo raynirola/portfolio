3c80b888e97d5b67cc936aed0bf9639acb379ebc
Ray Nirola <hi@raynirola.com>
2021-11-14 00:55:08 +0530

    NotificationProvider, useNotifications

---
+++ contexts/NotificationContext.tsx
@@ -0,0 +1,64 @@
+import React, { createContext, useCallback, useContext, useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+import { Notification } from '@/components/Notification'
+
+export const NotificationContext = createContext(null)
+
+export type NotificationType = {
+  id: string
+  message: string
+  title?: string
+  type: 'success' | 'error' | 'warning' | 'info' | string
+}
+
+export const NotificationProvider = ({ children }) => {
+  const [notifications, setNotifications] = useState<NotificationType[]>([])
+
+  useEffect(() => {
+    if (notifications.length > 0) {
+      const timeout = setTimeout(
+        () => setNotifications(notifications => notifications.slice(1)),
+        5000
+      )
+      return () => clearTimeout(timeout)
+    }
+  }, [notifications])
+
+  const addNotification = useCallback(
+    notification =>
+      setNotifications([...notifications, { ...notification, id: crypto.randomUUID() }]),
+    [notifications]
+  )
+
+  return (
+    <NotificationContext.Provider value={{ addNotification }}>
+      {children}
+      <Portal>
+        <div className="fixed top-4 right-4 w-max pointer-events-none z-20">
+          {notifications.map(notification => (
+            <Notification
+              key={notification.id}
+              title={notification.title}
+              message={notification.message}
+              type={notification.type}
+              id={notification.id}
+            />
+          ))}
+        </div>
+      </Portal>
+    </NotificationContext.Provider>
+  )
+}
+
+const Portal = ({ children }) => {
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => setMounted(true), [])
+  return mounted ? createPortal(children, document.body) : null
+}
+
+export const useNotification = () => {
+  const { addNotification } = useContext(NotificationContext)
+  return {
+    addNotification,
+  }
+}
