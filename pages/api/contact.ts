66c9fd9f2dfa242bb9a9b659db2c3f3d800d19c2
Ray Nirola <ray@nirola.tech>
2022-03-16 21:06:35 +0530

    Fix api early return

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -6,16 +6,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { name, email, message } = req.body
 
     if (!name || !email || !message) {
-      res.status(422).json({
-        error: 'Please provide a name, email and message'
+      return res.status(422).json({
+        error: 'Please provide a name, email and message',
       })
     }
 
     const emailPattern =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if (!emailPattern.test(String(email).toLowerCase())) {
-      res.status(422).json({
-        error: 'Please provide a valid email'
+      return res.status(422).json({
+        error: 'Please provide a valid email',
       })
     }
 
@@ -23,7 +23,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       from: `"${name}" <${process.env.FROM_EMAIL}>`,
       to: process.env.ADMIN_EMAIL as string,
       subject: 'Contact Message',
-      text: `From: \n${email} \n\n\nMessage: \n${message}`
+      text: `From: \n${email} \n\n\nMessage: \n${message}`,
     }
 
     try {
