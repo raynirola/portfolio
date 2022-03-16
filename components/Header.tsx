8b98448c9b54c3215a06cfda218bdaff18bdacda
Ray Nirola <ray@nirola.tech>
2022-03-16 19:50:10 +0530

    Removed darkmode for timebeing

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,14 @@
-import { Logo, ThemeToggleButton } from './index'
+import { Logo } from './index'
 
 export const Header = () => {
   return (
-    <header className="header sticky top-0 z-20 my-0 py-2 text-gray-900 dark:text-gray-100 md:my-8 md:py-8">
+    <header className="header sticky top-0 z-20 my-0 py-2 text-gray-900 dark:text-gray-100 md:my-6 md:py-4">
       <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-4xl lg:px-0 xl:max-w-5xl">
         <nav className="flex items-center justify-between">
           <Logo default />
-          <div className="flex items-center space-x-8">
+          {/* <div className="flex items-center space-x-8">
             <ThemeToggleButton />
-          </div>
+          </div> */}
         </nav>
       </div>
     </header>
