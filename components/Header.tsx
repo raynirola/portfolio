2100fa7e5857a48b855e0a8c243c675bbf0cea29
Ray Nirola <ray@nirola.tech>
2022-05-14 22:14:39 +0530

    Update logo in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,10 +5,7 @@ export const Header = () => {
     <header className="header sticky top-0 z-20 my-0 py-2 text-gray-900 dark:text-gray-100 md:my-6 md:py-4">
       <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-4xl lg:px-0 xl:max-w-5xl">
         <nav className="flex items-center justify-between">
-          <Logo default />
-          {/* <div className="flex items-center space-x-8">
-            <ThemeToggleButton />
-          </div> */}
+          <Logo />
         </nav>
       </div>
     </header>
